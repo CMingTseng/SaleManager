2d0ce22d28154fbd1ec407a17ef14e89ff84e0d2
MtViet <viet.ptit.17@gmail.com>
2016-06-27 00:11:09 +0700

    1

--- app/src/main/java/API/ServiceAPI.java
+++ app/src/main/java/API/ServiceAPI.java
@@ -6,12 +6,15 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.StrictMode;
 import android.provider.MediaStore;
+import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import okhttp3.MediaType;
@@ -21,6 +24,7 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import tvcompany.salemanager.model.Imagessss;
 import tvcompany.salemanager.model.User;
 
 /**
@@ -81,6 +85,24 @@ public class ServiceAPI {
                 t.printStackTrace();
             }
         });
+    }
+
+    public void SetImage(ImageView image)
+    {
+        Call<ResponseBody> call = git.getImage();
+        try{
+            if (android.os.Build.VERSION.SDK_INT > 9) {
+                StrictMode.ThreadPolicy policy =
+                        new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+            }
+            ResponseBody user = call.execute().body();
+
+            result="Nhận";
+        }
+        catch (Exception ex){
+            result= ex.toString();
+        }
     }
 
 }

746580ebb00ec9e7a0b4e8c20960d5d0db7ebaa7
Gandalf0303 <tung.ars150293@gmail.com>
2016-06-28 15:41:47 +0700

    Merge branch 'master' of https://github.com/TungVietCompany/SaleManager
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/tvcompany/salemanager/activity/LoginActivity.java
+++ app/src/main/java/tvcompany/salemanager/activity/LoginActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
+import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -50,7 +52,14 @@ public class LoginActivity extends AppCompatActivity {
 
         //Textview to link
         txtRegister = (TextView)  findViewById(R.id.txtRegister);
+        SpannableString content = new SpannableString("Đăng ký");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        txtRegister.setText(content);
+
         txtForget = (TextView)  findViewById(R.id.txtForget);
+        SpannableString content2 = new SpannableString("Quên mật khẩu");
+        content2.setSpan(new UnderlineSpan(), 0, content2.length(), 0);
+        txtForget.setText(content2);
 
         txtRegister.setOnClickListener(new View.OnClickListener() {
             @Override

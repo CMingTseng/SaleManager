f87a85cfca76f18a3d97fda6d422f12bf0a68f8f
Gandalf0303 <tung.ars150293@gmail.com>
2016-06-28 09:34:38 +0700

    Merge branch 'master' of https://github.com/TungVietCompany/SaleManager
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/tvcompany/salemanager/activity/LoginActivity.java
+++ app/src/main/java/tvcompany/salemanager/activity/LoginActivity.java
@@ -1,10 +1,13 @@
 package tvcompany.salemanager.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,13 +22,14 @@ import API.ServiceGenerator;
 import retrofit2.Call;
 import tvcompany.salemanager.R;
 import tvcompany.salemanager.controller.login.LoginController;
+import tvcompany.salemanager.library.SharedConstant;
 import tvcompany.salemanager.model.Status;
 import tvcompany.salemanager.model.User;
 
 
 public class LoginActivity extends AppCompatActivity {
     private LoginController loginController;
-
+    private CheckBox remember_me;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +37,21 @@ public class LoginActivity extends AppCompatActivity {
         final EditText txtName = (EditText) findViewById(R.id.editAccountLogin);
         final EditText txtPassword = (EditText) findViewById(R.id.editPassLogin);
         Button btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        remember_me=(CheckBox) findViewById(R.id.remember_me);
+        SharedPreferences sp = getSharedPreferences(
+                SharedConstant.LOGIN_STORE, Context.MODE_PRIVATE);
+        boolean logined = sp.getBoolean(SharedConstant.LOGINED_STAFF, false);
+        String username = sp.getString(SharedConstant.LOGIN_USERNAME, null);
+        String password = sp.getString(SharedConstant.LOGIN_PASSWORD, null);
+        if (username != null && password != null) {
+            txtName.setText(username);
+            txtPassword.setText(password);
+        } else {
+            txtName.setText("");
+            txtPassword.setText("");
+        }
+
         loginController = new LoginController();
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +61,26 @@ public class LoginActivity extends AppCompatActivity {
                     String password = txtPassword.getText().toString();
                     if (loginController.CheckLogin(userName, password)) {
                         Toast.makeText(LoginActivity.this, "OK", Toast.LENGTH_LONG).show();
+                        SharedPreferences sp  = LoginActivity.this.getSharedPreferences(
+                                SharedConstant.LOGIN_STORE, Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sp.edit();
+
+                        // Logined
+                        editor.putBoolean(SharedConstant.LOGINED_STAFF, true);
+                        editor.commit();
+
+                        // Save passwork
+                        if (remember_me.isChecked()) {
+                            editor.putString(SharedConstant.LOGIN_USERNAME,
+                                    userName);
+                            editor.putString(SharedConstant.LOGIN_PASSWORD,
+                                    password);
+                            editor.commit();
+                        } else {
+                            editor.putString(SharedConstant.LOGIN_USERNAME, "");
+                            editor.putString(SharedConstant.LOGIN_PASSWORD, "");
+                            editor.commit();
+                        }
                     }
 
                 } catch (Exception e) {

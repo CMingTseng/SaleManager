aece2f88ef7038bb573e26b1cfd069bddde5bfae
Gandalf0303 <tung.ars150293@gmail.com>
2016-06-29 15:44:39 +0700

    TUNGPM

--- app/src/main/java/tvcompany/salemanager/activity/ListOder.java
+++ app/src/main/java/tvcompany/salemanager/activity/ListOder.java
@@ -22,7 +22,7 @@ public class ListOder extends AppCompatActivity {
         lv = (ListView) findViewById(R.id.lv_oder);
         list =new ArrayList<Group>();
         for (int i = 0;i<50;i++){
-            Group group = new Group(R.drawable.image,"http://cdn.comedia.coccoc.com/2016-06-12/f8/d3/403f718a172533c52d85ee21d2da.jpg",null,null,null);
+            Group group = new Group(0,"http://cdn.comedia.coccoc.com/2016-06-12/f8/d3/403f718a172533c52d85ee21d2da.jpg",null,null,null);
             list.add(group);
         }
         adapter = new ListOderAdapter(list,ListOder.this);
